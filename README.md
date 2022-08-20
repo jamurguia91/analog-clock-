03c8842d5e6e1f915d55383f720c41eef03b4ef9
jamurguia91 <80859057+jamurguia91@users.noreply.github.com>
2022-08-20 09:45:57 -0700

    add code
    copied my code I did in codepen.io and pasted it here in vs code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# My-Boilerplate
-my custom boilerplate creation
+# Analog Clock 
+this was a video tutorial that I followed and styled certain things to my personal prefrence. 
--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-// my scripts
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
 
-console.log("hi");
+setInterval(() => {
+  
+let day = new Date();
+let hh = day.getHours() * 30;
+let mm = day.getMinutes() * deg;
+let ss = day.getSeconds() * deg;
+
+hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+mn.style.transform = `rotateZ(${mm}deg)`;
+sc.style.transform = `rotateZ(${ss}deg)`;
+  
+})
